5cb687795eefc145029ccc5004e7cb214cf36349
Artem <asustransformertf300x@gmail.com>
2019-07-08 20:11:42 +0300

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,8 @@
+function ShowForm(){
+   document.getElementById('popup').style.display = 'block';
+   document.getElementById('overlay').style.display = 'block';
+}
+function CloseForm(){
+   document.getElementById('popup').style.display = 'none';
+   document.getElementById('overlay').style.display = 'none';
+}
